c35a22e60f2f48c12debe01b05fa6f9ae9d0c023
codingdrizzle <francis.owusu@amalitech.com>
2024-01-04 23:46:55 +0000

    made a fix to api/config js

---
+++ src/api/config.js
@@ -0,0 +1,22 @@
+//import axios, { AxiosHeaders } from "axios";
+
+//let token = "";
+
+//if (typeof window !== "undefined") token = localStorage.getItem("token") || "";
+
+//export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+
+//console.log(API_BASE_URL)
+//export const default_headers = { authorization: `bearer ${token}`};
+
+//const API = axios.create({
+//    baseURL: API_BASE_URL,
+//    // headers: {
+//        // ...default_headers,
+//        "Content-Type": "application/json"
+//     // }
+//    // withCredentials: true,
+//});
+
+
+//export default API;
