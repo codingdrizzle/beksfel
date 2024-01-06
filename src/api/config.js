6f7e926e81732c969d6a0eb92b292ef1619bfae3
codingdrizzle <francis.owusu@amalitech.com>
2024-01-06 12:26:27 +0000

    fixed confirm button

--- src/api/config.js
+++ src/api/config.js
@@ -6,13 +6,14 @@ if (typeof window !== "undefined") token = localStorage.getItem("token") || "";
 
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
-export const default_headers = { authorization: `bearer ${token}`};
+export const default_headers = { authorization: `bearer ${token}` };
 
 const API = axios.create({
     baseURL: API_BASE_URL,
-     headers: {
-        "Content-Type": "application/json", 
-      }
+    headers: {
+        "Content-Type": "application/json",
+    },
+    timeout: 5000
 });
 
 
